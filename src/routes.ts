255e4c1ae3d1f6358b665e5c33b5b13bcd3a4e89
syncler <synclaprogrammer@gmail.com>
2023-09-30 02:37:29 -0300

    configurando o cadastro no banco

---
+++ src/routes.ts
@@ -0,0 +1,11 @@
+import {Router, Request, Response} from 'express'
+import { CreateUserController } from './controllers/user/CreateUserController';
+
+const router = Router();
+
+
+// -- rotas user --
+router.post('/users', new CreateUserController().handle )
+
+
+export {router};
